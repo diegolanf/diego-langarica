881d9d63939683d13f6613a3b1a18de77689b088
diegolanf <diego@langarica.me>
2023-11-12 22:06:31 +0100

    Define light and dark themes
    Other:
    - Set Roboto as default sans family

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };

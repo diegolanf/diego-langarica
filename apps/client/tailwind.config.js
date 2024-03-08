7c2b7438947cbc622d55cb53c32389daab946da2
diegolanf <diego@langarica.me>
2024-03-08 23:04:08 +0100

    Extract tailwind preset and move styles folder

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -3,27 +3,9 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('../../tailwind.preset.js')],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-      },
-      spacing: {
-        header: '3.5rem', // 56px
-        sm: '1.5rem', // 24px
-        md: '2rem', // 32px
-        lg: '2.5rem', // 40px
-        xl: '3rem', // 48px
-        'content-xs': '15rem', // 240px
-        'content-sm': '20rem', // 320px
-        'content-md': '25rem', //400px
-        'content-lg': '30rem', // 480px
-      },
-    },
-  },
-  plugins: [],
 };

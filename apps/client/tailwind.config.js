2f482a16bbde077839caf028d46171d0bce90e1a
diegolanf <diego@langarica.me>
2024-03-08 22:50:39 +0100

    Fix navigation styling

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
       },
+      spacing: {
+        header: '3.5rem', // 56px
+        sm: '1.5rem', // 24px
+        md: '2rem', // 32px
+        lg: '2.5rem', // 40px
+        xl: '3rem', // 48px
+        'content-xs': '15rem', // 240px
+        'content-sm': '20rem', // 320px
+        'content-md': '25rem', //400px
+        'content-lg': '30rem', // 480px
+      },
     },
   },
   plugins: [],

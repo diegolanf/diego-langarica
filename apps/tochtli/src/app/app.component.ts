f201e2cf5dfa6f68c16da960e3071a06baea8919
diegolanf <diego@langarica.me>
2024-03-10 11:35:29 +0100

    Fix Tochtli prefix

--- apps/tochtli/src/app/app.component.ts
+++ apps/tochtli/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'diego-langarica-root',
+  selector: 'tochtli-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

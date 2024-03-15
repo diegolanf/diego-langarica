929eb93398c721a9da647f49ac823b9d47cdb617
diegolanf <diego@langarica.me>
2024-03-15 19:39:36 +0100

    Create app init and define function

--- apps/home-api/src/main.ts
+++ apps/home-api/src/main.ts
@@ -1,22 +1,12 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import { nestApp } from './app/app.init';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await nestApp();
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
 }
 
 bootstrap();

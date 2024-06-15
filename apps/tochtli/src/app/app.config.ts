25a626294b792d93437b9c985abbbc8fa11c219b
diegolanf <diego@langarica.me>
2024-06-15 15:27:09 +0200

    Disable SSR in tochtli app

--- apps/tochtli/src/app/app.config.ts
+++ apps/tochtli/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { TranslocoHttpLoader } from '@dl/shared/infra/i18n';
@@ -13,9 +16,9 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideClientHydration(),
     provideEffects(),
     provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideStore(),
     provideTransloco({
